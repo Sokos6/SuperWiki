76b9331a447cef3796b60f7718ab7bd6bb724435
Shelby Escobedo <shelbyescobedo@SHelby-Escobedos-MacBook-Pro.local>
2016-02-12 15:16:38 -0700

    changed from mydb to superwiki

--- SuperWiki/src/data/SuperPersonType.java
+++ SuperWiki/src/data/SuperPersonType.java
@@ -5,6 +5,9 @@ import java.time.Year;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -12,6 +15,9 @@ import javax.persistence.Table;
 @Table(name="superpersontypes")
 public class SuperPersonType
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	@Enumerated(EnumType.STRING)
 	private SuperType superType;
 	@ManyToOne
