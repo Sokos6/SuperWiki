bcf13cd2f6aec3222c79c3bc399769141f3cb63d
Shelby Escobedo <shelbyescobedo@SHelby-Escobedos-MacBook-Pro.local>
2016-02-15 10:06:44 -0700

    retry spring

--- SuperWikiJPA/src/superHeroTest/User.java
+++ SuperWikiJPA/src/superHeroTest/User.java
@@ -8,60 +8,74 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user")
-public class User
-{
+@Table(name = "user")
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String username;
 	private String email;
 	private String password;
-	@Column(name="create_time")
+	@Column(name = "create_time")
 	private Date timeStamp;
-	@OneToMany(mappedBy="user")
+	@OneToMany(mappedBy = "user")
 	private List<Comment> comments;
-	
+	@ManyToMany
+	@JoinTable(name = "favorite_superpersons", joinColumns = @JoinColumn(name = "user_id") , inverseJoinColumns = @JoinColumn(name = "superpersons_id") )
+	private List<SuperPersons> favorites;
+
 	public int getId()
 	{
 		return id;
 	}
+
 	public void setId(int id)
 	{
 		this.id = id;
 	}
+
 	public String getUsername()
 	{
 		return username;
 	}
+
 	public void setUsername(String username)
 	{
 		this.username = username;
 	}
+
 	public String getEmail()
 	{
 		return email;
 	}
+
 	public void setEmail(String email)
 	{
 		this.email = email;
 	}
+
 	public String getPassword()
 	{
 		return password;
 	}
+
 	public void setPassword(String password)
 	{
 		this.password = password;
 	}
+
 	public Date getTimeStamp()
 	{
 		return timeStamp;
 	}
+
 	public void setTimeStamp(Date timeStamp)
 	{
 		this.timeStamp = timeStamp;
