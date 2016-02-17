dca16e384392d8405593ba1fe71edba3fba04c69
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-02-17 10:27:31 -0700

    hey

--- SuperWiki/src/dao/LoginDAO.java
+++ SuperWiki/src/dao/LoginDAO.java
@@ -8,12 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 import superHeroTest.User;
 
 @Transactional
-public class LoginDAO {
+public class LoginDAO
+{
 	@PersistenceContext
 	private EntityManager em;
+
 	public User getUser(String username, String password)
 	{
 		String query = "select u from User u where u.username = '" + username + "' and u.password = '" + password + "'";
-		return em.createQuery(query, User.class).getSingleResult(); 
+		return em.createQuery(query, User.class).getSingleResult();
 	}
 }
